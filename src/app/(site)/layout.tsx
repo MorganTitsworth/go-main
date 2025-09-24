2d87b89c79fb8cd14210910bdc5886809a7dc14f
Morgan <m.titsworth@erasolutions.us>
2025-09-24 16:45:36 -0700

    commit 2

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
           <AuthProvider>
             <ToasterContext />
             <Navbar />
+            asdasd
             {children}
             <Footer />
           </AuthProvider>
