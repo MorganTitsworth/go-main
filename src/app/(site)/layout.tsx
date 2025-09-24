a34da335d57600bccbdb88feb631044830b673f1
Morgan <m.titsworth@erasolutions.us>
2025-09-24 16:48:40 -0700

    commit removal

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           <AuthProvider>
             <ToasterContext />
             <Navbar />
-            asdasd
             {children}
             <Footer />
           </AuthProvider>
